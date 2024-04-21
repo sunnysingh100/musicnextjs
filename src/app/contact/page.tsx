ea72d643690221490caafb942079866c462a20fe
sunnysingh100 <sunnysingh706050@gmail.com>
2024-04-21 09:12:14 +0530

    next js project

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>dddddddddd</div>;
+}
+
+export default page;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, {useState} from "react";
+
+import {cn} from "@/utils/cn";
+import {Menu, MenuItem, HoveredLink} from "./ui/navbar-menu";
+import Link from "next/link";
+
+function Navbar({className}: {className?: string}) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            active={active}
+            setActive={setActive}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem active={active} setActive={setActive} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            active={active}
+            setActive={setActive}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
